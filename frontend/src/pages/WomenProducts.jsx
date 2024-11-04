9e9abe16b067245993a4a4cf64acc0524660c2aa
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-11-04 15:49:17 -0300

    WomenProducts 2.0

--- frontend/src/pages/WomenProducts.jsx
+++ frontend/src/pages/WomenProducts.jsx
@@ -1,6 +1,79 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Context from "../store/Context.jsx";
 
 const WomenProducts = () => {
+  const { store, actions } = useContext(Context);
+
+  const [isTshirt, setIsTshirt] = useState(false);
+  const [isSweaters, setIsSweaters] = useState(false);
+  const [isPants, setIsPants] = useState(false);
+  const [isDresses, setIsDresses] = useState(false);
+  const [isJeans, setIsJeans] = useState(false);
+  const [isPurses, setIsPurses] = useState(false);
+  const [isWallets, setIsWallets] = useState(false);
+  const [isBelts, setIsBelts] = useState(false);
+  const [isScarfs, setIsScarfs] = useState(false);
+  const [isPanties, setIsPanties] = useState(false);
+  const [isBras, setIsBras] = useState(false);
+  const [isPantys, setIsPantys] = useState(false);
+  const [isSocks, setIsSocks] = useState(false);
+
+  const turnClass = (info) => {
+    if (info.category == 1) {
+      return isTshirt ? "col" : "d-none";
+    } else if (info.category == 2) {
+      return isSweaters ? "col" : "d-none";
+    } else if (info.category == 4) {
+      return isPants ? "col" : "d-none";
+    } else if (info.category == 5) {
+      return isDresses ? "col" : "d-none";
+    } else if (info.category == 6) {
+      return isJeans ? "col" : "d-none";
+    } else if (info.category == 7) {
+      return isPurses ? "col" : "d-none";
+    } else if (info.category == 8) {
+      return isWallets ? "col" : "d-none";
+    } else if (info.category == 9) {
+      return isBelts ? "col" : "d-none";
+    } else if (info.category == 10) {
+      return isScarfs ? "col" : "d-none";
+    } else if (info.category == 11) {
+      return isPanties ? "col" : "d-none";
+    } else if (info.category == 12) {
+      return isBras ? "col" : "d-none";
+    } else if (info.category == 13) {
+      return isPantys ? "col" : "d-none";
+    } else if (info.category == 14) {
+      return isSocks ? "col" : "d-none";
+    }
+  };
+
+  const returnProducts = () => {
+    if (
+      !isTshirt &&
+      !isSweaters &&
+      !isPants &&
+      !isDresses &&
+      !isJeans &&
+      !isPurses &&
+      !isWallets &&
+      !isBelts &&
+      !isScarfs &&
+      !isPanties &&
+      !isBras &&
+      !isPantys &&
+      !isSocks
+    ) {
+      // Here map womenProducts from the flux context, and to map them use ProductCard component
+      // The ProductCard component is not made yet.
+      // Here the ProductCard class is going to be "col".
+    } else {
+      // Here map womenProducts from the flux context, and to map them use ProductCard component
+      // The ProductCard component is not made yet.
+      // Here the ProductCard class is going to be determined by turnClass function.
+    }
+  };
+
   return (
     <div className="container-fluid">
       <h1>Women Products</h1>
