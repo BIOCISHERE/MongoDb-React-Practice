949a7e05bbc317dead843ad6ac4164aff130bd2e
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-11-07 12:24:20 -0300

    ProductCard 1.0

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div className="container-fluid">
+      <h4>Product Card</h4>
+    </div>
+  );
+};
+
+export default ProductCard;
