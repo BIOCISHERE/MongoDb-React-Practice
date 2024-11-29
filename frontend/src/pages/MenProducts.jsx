e352754b61346b577bd104c7bef97453aa506ad5
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-11-29 15:35:58 -0300

    MenProducts 3.0

--- frontend/src/pages/MenProducts.jsx
+++ frontend/src/pages/MenProducts.jsx
@@ -85,7 +85,142 @@ const MenProducts = () => {
 
   return (
     <div className="container-fluid">
-      <h4>Men Products</h4>
+      <div className="row">
+        <div className="col-2 border border-dark-subtle rounded-end">
+          <h4>Menswear</h4>
+          <div className="container-fluid my-1">
+            <h6>Type of garment</h6>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="tshirtCheck"
+                onClick={() => setIsTShirt(!isTShirt)}
+              />
+              <label className="form-check-label" htmlFor="tshirtCheck">
+                T-shirts
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="sweatersCheck"
+                onClick={() => setIsSweaters(!isSweaters)}
+              />
+              <label className="form-check-label" htmlFor="sweatersCheck">
+                Sweaters
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="hoodiesCheck"
+                onClick={() => setIsHoodies(!isHoodies)}
+              />
+              <label className="form-check-label" htmlFor="hoodiesCheck">
+                Hoodies
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="pantsCheck"
+                onClick={() => setIsPants(!isPants)}
+              />
+              <label className="form-check-label" htmlFor="pantsCheck">
+                Pants
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="jeansCheck"
+                onClick={() => setIsJeans(!isJeans)}
+              />
+              <label className="form-check-label" htmlFor="jeansCheck">
+                Jeans
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="walletsCheck"
+                onClick={() => setIsWallets(!isWallets)}
+              />
+              <label className="form-check-label" htmlFor="walletsCheck">
+                Wallets
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="beltsCheck"
+                onClick={() => setIsBelts(!isBelts)}
+              />
+              <label className="form-check-label" htmlFor="beltsCheck">
+                Belts
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="scarfsCheck"
+                onClick={() => setIsScarfs(!isScarfs)}
+              />
+              <label className="form-check-label" htmlFor="scarfsCheck">
+                Scarfs
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="socksCheck"
+                onClick={() => setIsSocks(!isSocks)}
+              />
+              <label className="form-check-label" htmlFor="socksCheck">
+                Socks
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="underpantsCheck"
+                onClick={() => setIsUnderpants(!isUnderpants)}
+              />
+              <label className="form-check-label" htmlFor="underpantsCheck">
+                Underpants
+              </label>
+            </div>
+            <div className="form-check">
+              <input
+                className="form-check-input border border-dark"
+                type="checkbox"
+                id="boxersCheck"
+                onClick={() => setIsBoxers(!isBoxers)}
+              />
+              <label className="form-check-label" htmlFor="boxersCheck">
+                Boxers
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="col-10">
+          <div className="container-fluid">
+            <div className="row row-cols-xm-1 row-cols-sm-2 row-cols-md-4 g-4">
+              {/* Here use returnProducts func */}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
