e7a5cee81b16a3ec342d90843f21de678e3a520e
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-03 18:28:47 -0300

    FootwearProducts 2.0

--- frontend/src/pages/FootwearProducts.jsx
+++ frontend/src/pages/FootwearProducts.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import Context from "../store/Context.jsx";
+import ProductCard from "../components/ProductCard.jsx";
 
 const FootwearProducts = () => {
   const { store, actions } = useContext(Context);
@@ -22,6 +23,49 @@ const FootwearProducts = () => {
       return "col";
     }
   };
+
+  const returnProducts = () => {
+    if (!isUrban && !isSport && !isShoe && !isSandals) {
+      return store.footwearProducts.map((item, index) => (
+        <ProductCard
+          key={index}
+          cardClass="col"
+          cardId={item.id}
+          name={item.name}
+          price={item.price}
+          rating={item.rating}
+          ratingVotes={item.ratingVotes}
+        />
+      ));
+    } else {
+      return store.footwearProducts.map((item, index) => (
+        <ProductCard
+          key={index}
+          cardClass={turnClass(item)}
+          cardId={item.id}
+          name={item.name}
+          price={item.price}
+          rating={item.rating}
+          ratingVotes={item.ratingVotes}
+        />
+      ));
+    }
+  };
+
+  return (
+    <div className="container-fluid my-1">
+      <div className="row">
+        <div className="col-2 border border-dark-subtle rounded-end">
+          <h4>Footwear</h4>
+          <div className="container-fluid my-1">
+            <h6>Type of footwear</h6>
+            {/* Under this add the checkboxes */}
+          </div>
+        </div>
+        <div className="col-10"></div>
+      </div>
+    </div>
+  );
 };
 
 export default FootwearProducts;
