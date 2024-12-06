cd0d75a4cc325eb9de5d6f906497e1415d66c40d
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-06 14:40:16 -0300

    ShoppingCart 2.0

--- frontend/src/pages/ShoppingCart.jsx
+++ frontend/src/pages/ShoppingCart.jsx
@@ -7,9 +7,48 @@ const ShoppingCart = () => {
   const [isShippingRegion, setIsShippingRegion] = useState(1);
   const [isShowShipping, setIsShowShipping] = useState(false);
 
+  const returnShipping = () => {
+    if (isShippingRegion == 1) {
+      return 10;
+    } else if (isShippingRegion == 2) {
+      return 15;
+    } else if (isShippingRegion == 3) {
+      return 20;
+    } else if (isShippingRegion == 4) {
+      return 25;
+    } else if (isShippingRegion == 5) {
+      return 15;
+    } else if (isShippingRegion == 6) {
+      return 20;
+    } else {
+      return 15;
+    }
+  };
+
+  const returnShippingRegion = () => {
+    if (isShippingRegion == 1) {
+      return "North America";
+    } else if (isShippingRegion == 2) {
+      return "South America";
+    } else if (isShippingRegion == 3) {
+      return "Antartica";
+    } else if (isShippingRegion == 4) {
+      return "Africa";
+    } else if (isShippingRegion == 5) {
+      return "Europe";
+    } else if (isShippingRegion == 6) {
+      return "Asia";
+    } else {
+      return "Australia";
+    }
+  };
+
   return (
     <div className="container-fluid">
       <h2>Shopping Cart</h2>
+      {/* 
+        Before continuing, check if the shopping cart in the flux context if working.
+       */}
     </div>
   );
 };
