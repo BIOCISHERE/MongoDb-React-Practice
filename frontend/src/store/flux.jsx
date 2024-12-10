acc144d893166a4227f25c559a197c10f96feaac
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-10 12:37:00 -0300

    updated/fixed some functions in the flux context and also added commenst to the functions stored in the flux context

--- frontend/src/store/flux.jsx
+++ frontend/src/store/flux.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { FaRegStar, FaStar } from "react-icons/fa";
 
 const getState = ({ getStore, getActions, setStore }) => {
+  // In Store, we will save variables to be used globally in the project.
+  // In Actions, we will save functions to be used globally in the project.
   return {
     store: {
       test: "test",
@@ -30,13 +32,17 @@ const getState = ({ getStore, getActions, setStore }) => {
         }
       },
       linkManager: (num) => {
+        // We use this func to redirect the user to it's desired product.
         return `/products/${num}`;
       },
       returnFormated: (value) => {
+        // This func return the value with a coma every 3 digits.
         return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       },
       turnRating: (info) => {
+        // This func recives the rating, then it returns the rating but with "stars".
         if (info == 0) {
+          // 0 full stars, 5 empty stars.
           return (
             <>
               <FaRegStar />
@@ -47,6 +53,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             </>
           );
         } else if (info == 1) {
+          // 1 full stars, 4 empty stars.
           return (
             <>
               <FaStar />
@@ -57,6 +64,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             </>
           );
         } else if (info == 2) {
+          // 2 full stars, 3 empty stars.
           return (
             <>
               <FaStar />
@@ -67,6 +75,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             </>
           );
         } else if (info == 3) {
+          // 3 full stars, 2 empty stars.
           return (
             <>
               <FaStar />
@@ -77,6 +86,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             </>
           );
         } else if (info == 4) {
+          // 4 full stars, 1 empty stars.
           return (
             <>
               <FaStar />
@@ -87,6 +97,7 @@ const getState = ({ getStore, getActions, setStore }) => {
             </>
           );
         } else {
+          // 5 full stars, 0 empty stars.
           return (
             <>
               <FaStar />
@@ -103,11 +114,14 @@ const getState = ({ getStore, getActions, setStore }) => {
         let defaultCart = {};
         let defaultCarSize = {};
 
-        axios
+        // Fetching the products.
+        await axios
           .get(`http://localhost:8080/api/products/`)
           .then((products) => {
+            // Saving the products in the flux context.
             setStore({ fullResponse: products.data.data });
 
+            // Filtering the products into women, men and footwear products.
             store.fullResponse.map((product) => {
               if (product.productFor == "Women") {
                 let womenArray = store.womenProducts;
@@ -128,22 +142,27 @@ const getState = ({ getStore, getActions, setStore }) => {
                 setStore({ footwearProducts: footwearUpdate });
                 return true;
               } else {
+                // If a product fails to be filtered, then we print the error to the terminal with it's id.
                 console.error("filterProducts failed to set the store", product.id);
                 return false;
               }
             });
 
+            // After the fetch, we "build" a empty shopping cart with the products id.
             store.fullResponse.map((product) => {
               defaultCart[product.id] = 0;
             });
 
+            // After the fetch, we "build" a empty shopping cart to store the products sizes.
             store.fullResponse.map((product) => {
               defaultCarSize[product.id] = 0;
             });
 
+            // Saving the shopping carts in the flux context.
             setStore({ cart: defaultCart, cartSizes: defaultCarSize });
           })
           .catch((error) => {
+            // If there is a error, we catch it here.
             console.error(error);
             return false;
           });
@@ -152,6 +171,8 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         const actions = getActions();
 
+        // This func returns the total in the shopping cart of a specified products.
+        // Price times the amount of a products in the cart.
         const total = price * store.cart[id];
 
         return actions.returnFormated(total);
@@ -160,6 +181,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cart;
 
+        // This func adds 1 of a specified product to the shopping cart.
         updatedStore[itemId] += 1;
 
         setStore({ cart: updatedStore });
@@ -168,6 +190,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cart;
 
+        // This func adds a custom amount of a specified product to the shopping cart.
         updatedStore[itemId] += Number(num);
 
         setStore({ cart: updatedStore });
@@ -176,6 +199,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cart;
 
+        // This func subtract 1 of a speciefied product to the shopping cart.
         updatedStore[itemId] -= 1;
 
         setStore({ cart: updatedStore });
@@ -184,6 +208,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cart;
 
+        // This func resets the amount of a specified product to the shopping cart.
         updatedStore[itemId] = 0;
 
         setStore({ cart: updatedStore });
@@ -192,6 +217,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cart;
 
+        // This func sets a specified amount as the product amount to the shopping cart.
         updatedStore[itemId] = Number(num);
 
         setStore({ cart: updatedStore });
@@ -200,6 +226,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let totalCartPrice = 0;
 
+        // This func goes through the shopping cart and adds the price times the amount of a product to get the total.
         for (const item in store.cart) {
           if (store.cart[item] > 0) {
             let itemInfo = store.fullResponse.find((product) => product.id === Number(item));
@@ -213,6 +240,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let totalProduct = 0;
 
+        // This func returns the amount of products stored in the shopping cart.
         for (const item in store.cart) {
           totalProduct += store.cart[item];
         }
@@ -223,6 +251,8 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updatedStore = store.cartSizes;
 
+        // This func changes the stored size of a product.
+        // We use the cartSizes object to store the desired size of a product.
         updatedStore[id] = size;
 
         setStore({ cartSizes: updatedStore });
@@ -239,6 +269,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         const store = getStore();
         let updateShipping = store.shipping;
 
+        // This func changes the shipping region stored in the flux context.
         updateShipping = Number(num);
 
         setStore({ shipping: updateShipping });
