1a0f2868abcb39d1e03e139b7e7d47cc4177862d
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-20 13:17:48 -0300

    SignUp 2.0

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Context from "../store/Context.jsx";
 
 const SignUp = () => {
+  const { store, actions } = useContext(Context);
+
+  const [isEmail, setIsEmail] = useState("");
+  const [isPassword, setIsPassword] = useState("");
+  const [isConfirm, setIsConfirm] = useState("");
+  const [isShow, setIsShow] = useState(false);
+
+  const [isAlert, setIsAlert] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isMessage, setIsMessage] = useState("");
+
+  const redirect = useNavigate();
+
+  const redirectManager = () => {
+    setTimeout(() => {
+      redirect("/");
+    }, "5000");
+  };
   return (
     <div className="container-fluid">
       <h1>Sign up</h1>
