e278672833717c9a4cdb43bec2130ff81fc9f008
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-24 14:34:05 -0300

    SignUp 3.0

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -21,6 +21,57 @@ const SignUp = () => {
       redirect("/");
     }, "5000");
   };
+
+  const signUpRequest = () => {
+    {
+      /* Complete the backend of the users before continuing */
+    }
+  };
+
+  const registerInfo = () => {
+    if (isEmail == "") {
+      setIsAlert(true);
+      setIsMessage("Please type a email");
+      return false;
+    } else if (isPassword == "") {
+      setIsAlert(true);
+      setIsMessage("Please type a password");
+      return false;
+    } else if (isConfirm == "") {
+      setIsAlert(true);
+      setIsMessage("Please confirm your password");
+      return false;
+    } else if ((isPassword = !isConfirm)) {
+      setIsAlert(true);
+      setIsMessage("The passwords don't match");
+      return false;
+    } else {
+      // Here we must execute the fetch and delete the return below
+      return true;
+    }
+  };
+
+  const alertManager = () => {
+    if (isAlert) {
+      return (
+        <div className="container-fluid alert alert-danger text-center" role="alert">
+          {isMessage}
+        </div>
+      );
+    } else if (isSuccess) {
+      return (
+        <div className="container-fluid alert alert-success text-center" role="alert">
+          {isMessage}
+        </div>
+      );
+    } else {
+      return (
+        <div className="container-fluid alert alert-secondary text-center" role="alert">
+          Type your info
+        </div>
+      );
+    }
+  };
   return (
     <div className="container-fluid">
       <h1>Sign up</h1>
