322393661c86a7c40809057cf6cd9af09399aaed
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-25 17:34:02 -0300

    SignUp 4.0

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,77 +4,38 @@ import Context from "../store/Context.jsx";
 
 const SignUp = () => {
   const { store, actions } = useContext(Context);
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-  const [isEmail, setIsEmail] = useState("");
-  const [isPassword, setIsPassword] = useState("");
-  const [isConfirm, setIsConfirm] = useState("");
-  const [isShow, setIsShow] = useState(false);
-
-  const [isAlert, setIsAlert] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [isMessage, setIsMessage] = useState("");
-
-  const redirect = useNavigate();
-
-  const redirectManager = () => {
-    setTimeout(() => {
-      redirect("/");
-    }, "5000");
+  const registerUser = (e) => {
+    e.preventDefault();
   };
 
-  const signUpRequest = () => {
-    {
-      /* Complete the backend of the users before continuing */
-    }
-  };
-
-  const registerInfo = () => {
-    if (isEmail == "") {
-      setIsAlert(true);
-      setIsMessage("Please type a email");
-      return false;
-    } else if (isPassword == "") {
-      setIsAlert(true);
-      setIsMessage("Please type a password");
-      return false;
-    } else if (isConfirm == "") {
-      setIsAlert(true);
-      setIsMessage("Please confirm your password");
-      return false;
-    } else if ((isPassword = !isConfirm)) {
-      setIsAlert(true);
-      setIsMessage("The passwords don't match");
-      return false;
-    } else {
-      // Here we must execute the fetch and delete the return below
-      return true;
-    }
-  };
-
-  const alertManager = () => {
-    if (isAlert) {
-      return (
-        <div className="container-fluid alert alert-danger text-center" role="alert">
-          {isMessage}
-        </div>
-      );
-    } else if (isSuccess) {
-      return (
-        <div className="container-fluid alert alert-success text-center" role="alert">
-          {isMessage}
-        </div>
-      );
-    } else {
-      return (
-        <div className="container-fluid alert alert-secondary text-center" role="alert">
-          Type your info
-        </div>
-      );
-    }
-  };
   return (
     <div className="container-fluid">
-      <h1>Sign up</h1>
+      <div className="row">
+        <div className="col-6 border border-dark-subtle rounded mx-auto my-4">
+          <h1 className="text-center">Sign Up</h1>
+          {/* Here will be the alertManager */}
+          <form onSubmit={registerUser}>
+            <label htmlFor="dataName" className="form-label">
+              Name
+            </label>
+            <br />
+            <input
+              id="dataName"
+              type="email"
+              className="form-control"
+              placeholder="Enter name.."
+              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })}
+            />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
