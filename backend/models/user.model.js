09b88d25cfc66eb828bedef2e80924aeb332204f
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-27 15:27:28 -0300

    finished user.model.js in the backend

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -6,12 +6,57 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    lastname: {
+    email: {
       type: String,
       required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    favorites: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    adress: {
+      type: String,
+      required: false,
+    },
+    apartment: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: false,
+    },
+    state: {
+      type: String,
+      required: false,
+    },
+    city: {
+      type: String,
+      required: false,
+    },
+    postal: {
+      type: Number,
+      required: false,
     },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
