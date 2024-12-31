97e36ef4bfdc38eec3e33e644565051efe78acd8
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2024-12-31 15:17:08 -0300

    added registerUser func to the user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,3 +10,32 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name) {
+      res.status(400).json({ success: false, message: "Name is required" });
+    }
+
+    if (!password || password.length < 6) {
+      res.status(400).json({
+        success: false,
+        message: "Password is required and should be at least 6 characters long",
+      });
+    }
+
+    const doesExist = await User.findOne({ email });
+    if (doesExist) {
+      res.status(400).json({ success: false, message: "Email is already registered" });
+    }
+
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.status(201).json({ success: true, message: "User created successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
