9c1eae8e8145e1a51236a7094e19fb06df12d9ee
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-07 12:00:27 -0300

    LogIn 2.0

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,14 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Context from "../store/Context.jsx";
+import { redirect, useNavigate } from "react-router-dom";
 
 const LogIn = () => {
   const { store, actions } = useContext(Context);
 
+  const [isData, setIsData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isShow, setIsShow] = useState(false);
+
+  const navigate = useNavigate();
+
+  const redirectManager = () => {
+    setTimeout(() => {
+      redirect("/");
+    }, "3000");
+  };
+
+  const inputManager = () => {
+    if (isShow) {
+      return "text";
+    } else {
+      return "password";
+    }
+  };
+
+  const logInRequest = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col">
-          <h1>Log In </h1>
+        <div className="col-6 border border-dark-subtle rounded mx-auto my-4">
+          <h1 className="text-center">Log In </h1>
+          <form onSubmit={logInRequest}></form>
         </div>
       </div>
     </div>
