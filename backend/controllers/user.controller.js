81554e43c2e8ba35609457bfb2ddc8ef216cf5b2
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-09 12:29:48 -0300

    registerUser func in user.controller.js now has the password hashed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import { hashPassword } from "../auth.js";
 import User from "../models/user.model.js";
 
 export const getUsers = async (req, res) => {
@@ -14,28 +14,31 @@ export const getUsers = async (req, res) => {
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
+    // Check if name was entered
     if (!name) {
       return res.json({ success: false, message: "Name is required" });
     }
-
+    // Check if the email is a empty string
     if (email == "") {
       return res.json({ success: false, message: "Email is required" });
     }
-
+    // Check if email is already taken
     const doesExist = await User.findOne({ email });
     if (doesExist) {
       return res.json({ success: false, message: "Email is already registered" });
     }
-
+    // Check if the password is valid
     if (!password || password.length < 6) {
       return res.json({
         success: false,
         message: "Password is required and should be at least 6 characters long",
       });
     }
-
-    const newUser = new User({ name, email, password });
+    // Hashing the password
+    const hashedPassword = await hashPassword(password);
+    // Create new user
+    const newUser = new User({ name, email, password: hashedPassword });
+    // Save the new user.
     await newUser.save();
     return res.json({ success: true, message: "User created successfully" });
   } catch (error) {
