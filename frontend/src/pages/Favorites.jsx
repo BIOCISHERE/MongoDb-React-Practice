135b9e79004d6c0007cdf2364f7eeb16430113d2
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-23 14:18:27 -0300

    created Favorites.jsx

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Favorites = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <h1>Favorites</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Favorites;
