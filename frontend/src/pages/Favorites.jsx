30831beb09e330990476a2154bf67094d304887b
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-27 12:57:08 -0300

    Favorites 1.0

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -4,8 +4,19 @@ const Favorites = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col">
-          <h1>Favorites</h1>
+        <div className="col-10 border border-dark mx-auto">
+          <div className="row">
+            <div className="col text-center">
+              {/* If there is no user saved, the h1 tag should be just favorites with a prompt to login use see favorites */}
+              <h1>USERNAME's Favorites</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              {/* User favorites products will be mapped and each will be a card */}
+              <h3>Product card</h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
