40e2837f037d7894ab2d793d26896c3f760ae939
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-30 15:12:35 -0300

    Favorites 2.0

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import ProductCard from "../components/ProductCard.jsx";
 
 const Favorites = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-10 border border-dark mx-auto">
-          <div className="row">
-            <div className="col text-center">
-              {/* If there is no user saved, the h1 tag should be just favorites with a prompt to login use see favorites */}
-              <h1>USERNAME's Favorites</h1>
-            </div>
+        <div className="col-10 mx-auto">
+          <div
+            className="container-fluid text-center"
+            style={{ marginTop: "2rem", marginBottom: "2rem" }}
+          >
+            <h1>USERNAME's Favorites</h1>
           </div>
-          <div className="row">
-            <div className="col">
-              {/* User favorites products will be mapped and each will be a card */}
-              <h3>Product card</h3>
+          <div className="container-fluid">
+            <div className="row border border-dark-subtle rounded row-cols-2 row-cols-md-4 gx-4 my-3">
+              {/* 
+              These ProductCard components are just examples
+              Later the ProductCard components will be mapped 
+            */}
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
+              <ProductCard cardClass="col" name="test" price="10" rating="4" ratingVotes="4" />
             </div>
           </div>
         </div>
