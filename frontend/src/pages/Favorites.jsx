3d1186fd049e08219d15cf5f1eb664a22713ecb2
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-01-31 11:02:28 -0300

    Favorites 3.0

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from "../components/ProductCard.jsx";
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
   return (
@@ -11,6 +12,17 @@ const Favorites = () => {
             style={{ marginTop: "2rem", marginBottom: "2rem" }}
           >
             <h1>USERNAME's Favorites</h1>
+            <span>Want to add more products?</span>
+            <br />
+            <Link to="/women-products" className="mx-1 fauxLetters">
+              Women Products
+            </Link>
+            <Link to="/men-products" className="mx-1 fauxLetters">
+              Men Products
+            </Link>
+            <Link to="/footwear-products" className="mx-1 fauxLetters">
+              Footwear Products
+            </Link>
           </div>
           <div className="container-fluid">
             <div className="row border border-dark-subtle rounded row-cols-2 row-cols-md-4 gx-4 my-3">
