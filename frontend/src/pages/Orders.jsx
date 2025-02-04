38168ca77a997ad55e28ae47c8d58f84dbf039ef
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-02-04 15:43:33 -0300

    created Orders.jsx

---
+++ frontend/src/pages/Orders.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Orders = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <h1>Orders</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
