53d691d32efa9a7cc8f0e9893592141110281720
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-02-06 13:51:29 -0300

    Orders 1.0

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -4,8 +4,13 @@ const Orders = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col">
-          <h1>Orders</h1>
+        <div className="col-10 mx-auto">
+          <div className="container-fluid text-center">
+            <h1>My Orders</h1>
+          </div>
+          <div className="container-fluid border border-dark-subtle">
+            <h4>Mapped order/product</h4>
+          </div>
         </div>
       </div>
     </div>
