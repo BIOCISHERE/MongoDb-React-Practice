3d9f520ddf4bc83d845ffc26dc8be50a255087d0
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-03-19 17:21:41 -0300

    Shipping 2.0, in Shipping.jsx

--- frontend/src/pages/Shipping.jsx
+++ frontend/src/pages/Shipping.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Shipping = () => {
+  const [isShipping, setIsShipping] = useState({
+    adress: "",
+    apartment: "",
+    country: "",
+    state: "",
+    city: "",
+    postal: "",
+  });
   return (
     <div className="container-fluid">
-      <h1>Shipping</h1>
+      <div className="row">
+        <div className="col-10 mx-auto">
+          <h1>Shipping</h1>
+        </div>
+      </div>
     </div>
   );
 };
