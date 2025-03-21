cdcd7bdbf2002e1ee1c78592356093880f0c0bf9
BIOC_IS_HERE <bioc.is.here.8@gmail.com>
2025-03-21 15:28:50 -0300

    added first input in Shipping.jsx

--- frontend/src/pages/Shipping.jsx
+++ frontend/src/pages/Shipping.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const Shipping = () => {
-  const [isShipping, setIsShipping] = useState({
+  const [isData, setIsData] = useState({
     adress: "",
     apartment: "",
     country: "",
@@ -20,7 +20,20 @@ const Shipping = () => {
           <h1 className="text-center">Shipping</h1>
           <form onSubmit={shippingRequest}>
             <div className="mb-3">
-              <p>input</p>
+              <label htmlFor="dataAdress" className="form-label ms-2">
+                Adress
+              </label>
+              <input
+                id="dataAdress"
+                type="text"
+                className="form-control border border-dark-subtle"
+                placeholder="Enter adress ..."
+                value={isData.adress}
+                onChange={(e) => setIsData({ ...isData, adress: e.target.value })}
+              />
+            </div>
+            <div className="mb-3">
+              <h1>next input</h1>
             </div>
           </form>
         </div>
